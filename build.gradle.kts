36d2b58c51ffb63070ad02b898a25b06547933d2
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2023-08-24 14:11:56 -0500

    remove codecov

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.*
 
+// Import Jacoco plugin
+
 plugins {
 	id("io.spring.dependency-management") version "1.1.3"
 	id("org.springframework.boot") version "3.1.3"
